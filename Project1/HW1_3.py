4d736bcf16d6aa5fa675c5a23971492cbb738c3d
Meet Taraviya <meet.taraviya@cs.ucla.edu>
2020-01-15 21:54:38 -0800

    semi working q3,q4

--- Project1/HW1_3.py
+++ Project1/HW1_3.py
@@ -2,18 +2,17 @@
 #Problem 3
 #LSI
 import numpy as np
-from scipy.linalg import svd
+from numpy.linalg import svd
 
 U, s, VT = svd(X) #X is TF-IDF matrix from problem 2
 # s[::-1].sort() # use this method or the one below
--np.sort(-s)
 
 V = VT.transpose()
 k = 50
 Uk = U[:,:k]
 Vk = V[:,:k]
 
-np.matmul(X,Vk)
+X_lsa = np.matmul(X,Vk)
 
 #NMF
 # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html
@@ -21,6 +20,8 @@ from sklearn.decomposition import NMF
 model = NMF(n_components=k, init='random', random_state=0)
 W = model.fit_transform(X)
 H = model.components_
+
+X_nmf = np.matmul(W, H)
 # not sure if we need to solve a least squared problem or if the NMF function takes care of it
 
 # Compare the norms of NMF and LSI
